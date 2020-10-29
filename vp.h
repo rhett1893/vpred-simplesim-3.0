e732e488bd24c6c2e0a90cf9cecc735108f0ae71
Rhett Hanscom <rhett.hanscom@colorado.edu>
2020-10-29 10:50:53 -0600

    preliminary compilation successful - not well tested, no functionality implemented.

--- vp.h
+++ vp.h
@@ -1,5 +1,5 @@
-#ifndef _stride_h_ 
-#define _stride_h_ 
+#ifndef _stride_h_
+#define _stride_h_
 
 /* constants for entry lookup, update status in stride table: */
 /* MISS - not found */
@@ -17,7 +17,7 @@
 #define DOUBLE		1
 
 /* pc stride (in bits) */
-#define INST_OFFSET	3	
+#define INST_OFFSET	3
 
 /* Single precision table entry definition */
 typedef struct ln_i1{
@@ -26,7 +26,7 @@ typedef struct ln_i1{
   int stride;           /* the stride between the two known last values */
   int last_alloc;       /* allocation sim_cycle time */
   int last_ref;         /* last lookup sim_cycle time */
-  char ps;              /* classification fsm present state: 
+  char ps;              /* classification fsm present state:
 			             0,1 - don't use prediction (don't go)
 			             1,2 - use prediction (go)           */
   char valid;           /* valid bit */
@@ -34,9 +34,9 @@ typedef struct ln_i1{
 			   when using X mechanism predicted value is not:
 			   last_val + stride, but: last_val + X*stride.
 			   X is incremented with every fetch (lookup) and
-			   decremented when the instruction leaves (either 
+			   decremented when the instruction leaves (either
 			   by normal terminition (update) or when instruction
-			   doesn't exit via commit, i.e. pipe is flushed 
+			   doesn't exit via commit, i.e. pipe is flushed
 			   (lookup_undo) */
 } Hash_i1, *Hash_ptr_i1;
 
@@ -53,7 +53,7 @@ typedef struct ln_i2{
 } Hash_i2, *Hash_ptr_i2;
 
 extern int                       en_lookup_stride;
-extern SS_TIME_TYPE              sim_cycle;
+// extern SS_TIME_TYPE              sim_cycle;
 extern int                       use_stride;
 extern int                       use_fsm;
 extern int                       use_vp;
@@ -70,21 +70,12 @@ extern int                       predicted_ok;
 
 void create_stride();
 
-int update(SS_ADDR_TYPE instpc,SS_INST_TYPE inst,VAL_TAG_TYPE pred_val,VAL_TAG_TYPE calc_val, int my_ref);
-
-int lookup_undo(SS_ADDR_TYPE instpc, SS_INST_TYPE inst, VAL_TAG_TYPE pred_val);
-
-int allocate(SS_ADDR_TYPE pred_PC,SS_INST_TYPE inst,VAL_TAG_TYPE calc_val,int my_ref);
-
-void lookup(SS_ADDR_TYPE pred_PC,SS_INST_TYPE inst,VAL_TAG_TYPE *pred_val);
+// int update(SS_ADDR_TYPE instpc,SS_INST_TYPE inst,VAL_TAG_TYPE pred_val,VAL_TAG_TYPE calc_val, int my_ref);
+//
+// int lookup_undo(SS_ADDR_TYPE instpc, SS_INST_TYPE inst, VAL_TAG_TYPE pred_val);
+//
+// int allocate(SS_ADDR_TYPE pred_PC,SS_INST_TYPE inst,VAL_TAG_TYPE calc_val,int my_ref);
+//
+// void lookup(SS_ADDR_TYPE pred_PC,SS_INST_TYPE inst,VAL_TAG_TYPE *pred_val);
 
 #endif
-
-
-
-
-
-
-
-
-
