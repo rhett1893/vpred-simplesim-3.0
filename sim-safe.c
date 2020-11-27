3451e68b58e6d0fe39cc3c2f0a5db554ea91cece
Rhett Hanscom <rhett.hanscom@colorado.edu>
2020-11-27 15:39:12 -0700

    fixed 'use_vp' error

--- sim-safe.c
+++ sim-safe.c
@@ -2,20 +2,20 @@
 
 /* SimpleScalar(TM) Tool Suite
  * Copyright (C) 1994-2003 by Todd M. Austin, Ph.D. and SimpleScalar, LLC.
- * All Rights Reserved. 
- * 
+ * All Rights Reserved.
+ *
  * THIS IS A LEGAL DOCUMENT, BY USING SIMPLESCALAR,
  * YOU ARE AGREEING TO THESE TERMS AND CONDITIONS.
- * 
+ *
  * No portion of this work may be used by any commercial entity, or for any
  * commercial purpose, without the prior, written permission of SimpleScalar,
  * LLC (info@simplescalar.com). Nonprofit and noncommercial use is permitted
  * as described below.
- * 
+ *
  * 1. SimpleScalar is provided AS IS, with no warranty of any kind, express
  * or implied. The user of the program accepts full responsibility for the
  * application of the program and the use of any results.
- * 
+ *
  * 2. Nonprofit and noncommercial use is encouraged. SimpleScalar may be
  * downloaded, compiled, executed, copied, and modified solely for nonprofit,
  * educational, noncommercial research, and noncommercial scholarship
@@ -24,13 +24,13 @@
  * solely for nonprofit, educational, noncommercial research, and
  * noncommercial scholarship purposes provided that this notice in its
  * entirety accompanies all copies.
- * 
+ *
  * 3. ALL COMMERCIAL USE, AND ALL USE BY FOR PROFIT ENTITIES, IS EXPRESSLY
  * PROHIBITED WITHOUT A LICENSE FROM SIMPLESCALAR, LLC (info@simplescalar.com).
- * 
+ *
  * 4. No nonprofit user may place any restrictions on the use of this software,
  * including as modified by the user, by any other authorized user.
- * 
+ *
  * 5. Noncommercial and nonprofit users may distribute copies of SimpleScalar
  * in compiled or executable form as set forth in Section 2, provided that
  * either: (A) it is accompanied by the corresponding machine-readable source
@@ -40,11 +40,11 @@
  * must permit verbatim duplication by anyone, or (C) it is distributed by
  * someone who received only the executable form, and is accompanied by a
  * copy of the written offer of source code.
- * 
+ *
  * 6. SimpleScalar was developed by Todd M. Austin, Ph.D. The tool suite is
  * currently maintained by SimpleScalar LLC (info@simplescalar.com). US Mail:
  * 2395 Timbercrest Court, Ann Arbor, MI 48105.
- * 
+ *
  * Copyright (C) 1994-2003 by Todd M. Austin, Ph.D. and SimpleScalar, LLC.
  */
 
@@ -91,7 +91,7 @@ md_inst_t inst;
 VAL_TAG_TYPE pred_val_main;
 extern int common_ref;
 extern int misses_vpred;
-int use_vp=1;
+extern int use_vp=1;
 extern int lookup_accessed;
 extern int update_accessed;
 extern int allocate_accessed;
@@ -100,7 +100,7 @@ extern int found_value;
 void
 sim_reg_options(struct opt_odb_t *odb)
 {
-  opt_reg_header(odb, 
+  opt_reg_header(odb,
 "sim-safe: This simulator implements a functional simulator.  This\n"
 "functional simulator is the simplest, most user-friendly simulator in the\n"
 "simplescalar tool set.  Unlike sim-fast, this functional simulator checks\n"
@@ -128,7 +128,7 @@ sim_check_options(struct opt_odb_t *odb, int argc, char **argv)
 /* register simulator-specific statistics */
 void
 sim_reg_stats(struct stat_sdb_t *sdb)
-{ 
+{
   stat_reg_counter(sdb, "sim_num_insn",
 		   "total number of instructions executed",
 		   &sim_num_insn, sim_num_insn, NULL);
--- vp.c
+++ vp.c
@@ -29,9 +29,9 @@ int                       vp_replace;       /* Value prediction replace policy*/
 int                       start_fsm;        /* FSM intial state */
 int                       hash_no_i1;       /* Single precision set number */
 int                       hash_no_i2;       /* Double precision set number */
-int                       hash_asso_i1;     /* Single precision set 
+int                       hash_asso_i1;     /* Single precision set
 					       associativity */
-int                       hash_asso_i2;     /* Double precision set 
+int                       hash_asso_i2;     /* Double precision set
 					       associativity */
 counter_t predicted_ok=0;     /* total number of correct predictions */
 counter_t Wrong_Predictions=0;     /* total number of correct predictions */
@@ -43,14 +43,14 @@ static Hash_ptr_i1       *hash_table_i1;    /* pointers to VP tables */
 static Hash_ptr_i2       *hash_table_i2;
 
 
-/*allocates the memory needed for the Value Prediction Tables*/ 
+/*allocates the memory needed for the Value Prediction Tables*/
 void create_stride()
 {
 	register i;
 	hash_no_i1 = MEM_PTAB_SIZE;
   hash_asso_i1 = 4;
-	if(use_vp==0) /* check if value prediction is used */
-	  return;	
+	if(&use_vp==0) /* check if value prediction is used */
+	  return;
 	if((hash_table_i1=(Hash_ptr_i1 *) calloc (hash_no_i1,sizeof(Hash_ptr_i1)))==0){
 		fprintf(stderr,"out of memory. terminating program.\n");  /*calloc error*/
 		exit(-1);
@@ -70,9 +70,9 @@ void create_stride()
 			exit(-1);
 		}
 
-} /* end of create_stride */   
+} /* end of create_stride */
 
-/* update an instruction in the Vp table (update output outcomes)*/ 
+/* update an instruction in the Vp table (update output outcomes)*/
 int update(
  md_addr_t instpc, /* The instruction address */
  md_inst_t inst,   /* The instruction opcode and registers */
@@ -88,65 +88,65 @@ int update(
   char flag[2];        /* single/double precision correct prediction flags */
   char found=0;        /* inst "found" status */
   enum md_opcode op;   /* decoded opcode enum */
- 
-  if(use_vp==0) /* check if using VP */
-     return(MISS);	
-  pc = (unsigned int)(instpc>>INST_OFFSET); 
+
+  if(&use_vp==0) /* check if using VP */
+     return(MISS);
+  pc = (unsigned int)(instpc>>INST_OFFSET);
   flag[0]=0;
-  flag[1]=0; 
-  MD_SET_OPCODE(op, inst);  /* recover opcode from inst */  
-  //if(SS_OP_FLAGS(op)&F_SINGLE_P){ 
+  flag[1]=0;
+  MD_SET_OPCODE(op, inst);  /* recover opcode from inst */
+  //if(SS_OP_FLAGS(op)&F_SINGLE_P){
     /* single precision output */
      val[0]=calc_val.value.single_p;
      index = (unsigned int)(pc % hash_no_i1); /* find table entry */
-     for(i=0;i<hash_asso_i1;i++)  
+     for(i=0;i<hash_asso_i1;i++)
        /* find inst in set */
          if((hash_table_i1[index][i].pc==pc)&&(hash_table_i1[index][i].valid)){
 	    found=1;
 	    break;
-	 }			
+	 }
      if(!found)                   /*miss*/
 	return(MISS);
-     else{  
-        if(val[0]==pred_val.value.single_p){ 
+     else{
+        if(val[0]==pred_val.value.single_p){
               /*hit*/
 	   predicted_ok++;       /* increase total predicated ok counter */
 	   /* update the classification fsm state */
-	   if(hash_table_i1[index][i].ps<3) 
+	   if(hash_table_i1[index][i].ps<3)
 	      hash_table_i1[index][i].ps++;
-	   flag[0]=1;                      
+	   flag[0]=1;
 	   hash_table_i1[index][i].pred_correct++; /* statistics */
 	 }
          else{ /* hit fault: found, but wrong prediction was given */
 	   /* update the classification fsm state */
 	 	Wrong_Predictions++;
-	    if(hash_table_i1[index][i].ps>0)  
+	    if(hash_table_i1[index][i].ps>0)
 	      hash_table_i1[index][i].ps--;
 
-	    
+
 	 }
 
        hash_table_i1[index][i].accessed++;    /* statistics */
 
        if (hash_table_i1[index][i].x<1)
 	   panic("invalid X value (lookup stride)");
-       
+
        /* X is decreased only if:
 	   A. X was increased in lookup (there was a lookup hit).
 	   B. X > 1.   */
-       if ((hash_table_i1[index][i].x>1)&&(pred_val.fsm_pred!=MISS)) 
+       if ((hash_table_i1[index][i].x>1)&&(pred_val.fsm_pred!=MISS))
 	 hash_table_i1[index][i].x--;     /* lookup X mechanism update */
 
        /* stride, last_val, last_ref are updated only in order */
 
        if (hash_table_i1[index][i].last_ref<my_ref) {
-         /* update stride value */ 
+         /* update stride value */
          hash_table_i1[index][i].stride=val[0]-hash_table_i1[index][i].last_val;
          hash_table_i1[index][i].last_ref=my_ref;
          hash_table_i1[index][i].last_val=val[0]; /* update last value */
        }
        /* else:
-	    there was already an update of a command that appears later 
+	    there was already an update of a command that appears later
 	    in the code (sequentally) - stride, last_val, last_ref are not
 	    updated */
 
@@ -171,8 +171,8 @@ int update(
 	// }
  //     if(!found)                   /*miss*/
 	// return(MISS);
- //     else{               
-	// for(j=0;j<2;j++) 
+ //     else{
+	// for(j=0;j<2;j++)
  //         if(val[j]==pred_val.value.double_p[j]){
  //            /*hit*/
 	//       predicted_ok++; /* statistics */
@@ -183,9 +183,9 @@ int update(
  //        hash_table_i2[index][i].accessed++; /* statistics */
 
 	// if (hash_table_i2[index][i].x<1)
- // 	   panic("invalid X value (lookup stride)"); 	
+ // 	   panic("invalid X value (lookup stride)");
 
- //        if ((hash_table_i2[index][i].x>1)&&(pred_val.fsm_pred!=MISS)) 
+ //        if ((hash_table_i2[index][i].x>1)&&(pred_val.fsm_pred!=MISS))
 	//    hash_table_i2[index][i].x--;    /* update lookup X mechanism*/
 
  //        if (hash_table_i2[index][i].last_ref<my_ref){
@@ -199,11 +199,11 @@ int update(
 
 	//   /* update last_ref */
 	//   hash_table_i2[index][i].last_ref = my_ref;
- //        }  
+ //        }
 
 
 	//    /* update the classification fsm state */
- //        if((flag[0]==1)&&(flag[1]==1)){ 
+ //        if((flag[0]==1)&&(flag[1]==1)){
 	//    if(hash_table_i2[index][i].ps<3)
 	//       hash_table_i2[index][i].ps++;
 	//    return(HIT);
@@ -222,32 +222,32 @@ int update(
 int lookup_undo(
  md_addr_t instpc,/* The instruction address */
  md_inst_t inst,  /* The instruction opcode and registers */
- VAL_TAG_TYPE pred_val  /* The instruction predicted value and flags */ ) 
+ VAL_TAG_TYPE pred_val  /* The instruction predicted value and flags */ )
 {
   unsigned int index;
   unsigned int pc;
   register i;
   char found=0;
   enum md_opcode op; /* decoded opcode enum */
- 
-  if(use_vp==0)
-     return(MISS);	
-  pc = (unsigned int)(instpc>>INST_OFFSET); 
-  MD_SET_OPCODE(op, inst); 
+
+  if(&use_vp==0)
+     return(MISS);
+  pc = (unsigned int)(instpc>>INST_OFFSET);
+  MD_SET_OPCODE(op, inst);
   //if(SS_OP_FLAGS(op)&F_SINGLE_P){
      index = (unsigned int)(pc % hash_no_i1);
      for(i=0;i<hash_asso_i1;i++)
          if((hash_table_i1[index][i].pc==pc)&&(hash_table_i1[index][i].valid)){
 	    found=1;
 	    break;
-	 }			
+	 }
      if(!found)                   /*miss*/
 	return(MISS);
-     else{  
+     else{
        if (hash_table_i1[index][i].x<1)
 	   panic("invalid X value (lookup stride)");
 
-       if ((hash_table_i1[index][i].x>1)&&(pred_val.fsm_pred!=MISS)) 
+       if ((hash_table_i1[index][i].x>1)&&(pred_val.fsm_pred!=MISS))
 	   hash_table_i1[index][i].x--;
        return(HIT);
      }
@@ -261,11 +261,11 @@ int lookup_undo(
 	// }
  //     if(!found)                   /*miss*/
 	// return(MISS);
- //     else{               
+ //     else{
 	// if (hash_table_i2[index][i].x<1)
- // 	   panic("invalid X value (lookup stride)"); 
-        
- //        if ((hash_table_i2[index][i].x>1)&&(pred_val.fsm_pred!=MISS)) 
+ // 	   panic("invalid X value (lookup stride)");
+
+ //        if ((hash_table_i2[index][i].x>1)&&(pred_val.fsm_pred!=MISS))
 	//     hash_table_i2[index][i].x--;
 	// return(HIT);
  //     }
@@ -273,10 +273,10 @@ int lookup_undo(
   //else return(MISS);
 }
 
-/* find a replacment candidate. Replacement policy: 
-   LRU */ 
+/* find a replacment candidate. Replacement policy:
+   LRU */
 unsigned int find_new_place_lru(
-int index, /* the table entry in which the record should be place */ 
+int index, /* the table entry in which the record should be place */
 char data_type /* single or double presicion */)
 {
   unsigned int i;
@@ -286,7 +286,7 @@ char data_type /* single or double presicion */)
   switch(data_type){
 	  case SINGLE:
 	                 /* init min_ref */
-			 min_ref=hash_table_i1[index][0].last_ref; 
+			 min_ref=hash_table_i1[index][0].last_ref;
 			 new_place=0;
 			 for(i=0;i<hash_asso_i1;i++){
 				 if(hash_table_i1[index][i].valid == 0)
@@ -318,7 +318,7 @@ char data_type /* single or double presicion */)
 
 /* find a replacment candidate. Replacment policy:
    find all the entries with the (same) lowest fsm state (these are less
-   predictable insts). from all these replace the LRU entry. */ 
+   predictable insts). from all these replace the LRU entry. */
 unsigned int find_new_place_fsm(int index, char data_type)
 {
   unsigned int i;
@@ -385,14 +385,14 @@ unsigned int replace;  /* the record in which the pc is allocated */
 //register j;
 char data_type;        /* instruction precision */
 enum md_opcode op;     /* instruction opcode */
-  
+
 /* using Value Prediction ? */
-if(use_vp==0)     
-   return;	
+if(&use_vp==0)
+   return;
 MD_SET_OPCODE(op, inst);
 pc = (unsigned int)(pred_PC)>>INST_OFFSET;
 
-// if(SS_OP_FLAGS(op)&F_SINGLE_P){  
+// if(SS_OP_FLAGS(op)&F_SINGLE_P){
   /* single precision */
    val[0]=calc_val.value.single_p;
    data_type=SINGLE;
@@ -404,7 +404,7 @@ pc = (unsigned int)(pred_PC)>>INST_OFFSET;
 
    /* update the entry fields */
    hash_table_i1[index][replace].pc = pc;
-   hash_table_i1[index][replace].ps = start_fsm; 
+   hash_table_i1[index][replace].ps = start_fsm;
    hash_table_i1[index][replace].x = 1;
    hash_table_i1[index][replace].last_val = val[0];
    hash_table_i1[index][replace].stride = 0;
@@ -413,10 +413,10 @@ pc = (unsigned int)(pred_PC)>>INST_OFFSET;
    hash_table_i1[index][replace].last_alloc = (int) sim_cycle;
    hash_table_i1[index][replace].last_ref = (int) my_ref;
    hash_table_i1[index][replace].valid=1;
-//} 
+//}
 // else if(SS_OP_FLAGS(op)&F_DOUBLE_P){
-//    val[0]=calc_val.value.double_p[0]; 
-//    val[1]=calc_val.value.double_p[1]; 
+//    val[0]=calc_val.value.double_p[0];
+//    val[1]=calc_val.value.double_p[1];
 //    index=(unsigned int)(pc % hash_no_i2);
 //    data_type=DOUBLE;
 
@@ -444,32 +444,32 @@ pc = (unsigned int)(pred_PC)>>INST_OFFSET;
 void lookup(md_addr_t pred_PC,/* The instruction address */
 md_inst_t inst, /* The instruction calculated value and flags */
 VAL_TAG_TYPE *pred_val,   /* The instruction predicted value and flags */
-struct mem_t *mem  /* The memory block to be accessed for reading the value */ ) 
+struct mem_t *mem  /* The memory block to be accessed for reading the value */ )
 {
   lookup_accessed++;
   unsigned int index; /* VP table index*/
   unsigned int pc;    /* instruction shifted address */
   int val[2];
-  
+
   register i;
   char X;             /* X (lookup stride) mechanisim value */
-  // char flag[2];      
+  // char flag[2];
   char found=0;
   enum md_opcode op;  /* decoded opcode enum */
 
-  pred_val->fsm_pred=MISS;  
-  
+  pred_val->fsm_pred=MISS;
+
   /* using Value Prediction ? */
-  if(use_vp==0)
+  if(&use_vp==0)
      return;
   pc = (unsigned int)(pred_PC>>INST_OFFSET); /* shifting the instruction
-                                                address */ 
-   
-  // flag[0]=0;  clear flags 
-  // flag[1]=0; 
+                                                address */
+
+  // flag[0]=0;  clear flags
+  // flag[1]=0;
   MD_SET_OPCODE(op, inst); /* recover op code from inst */
 
-  //if(SS_OP_FLAGS(op)&F_SINGLE_P){ 
+  //if(SS_OP_FLAGS(op)&F_SINGLE_P){
     /* single precision */
      index = (unsigned int)(pc % hash_no_i1);
      for(i=0;i<hash_asso_i1;i++)
@@ -477,11 +477,11 @@ struct mem_t *mem  /* The memory block to be accessed for reading the value */ )
          if((hash_table_i1[index][i].pc==pc)&&(hash_table_i1[index][i].valid)){
 	    found=1;
 	    break;
-	 }			
+	 }
      if(!found)                   /*miss*/
 	{  pred_val->fsm_pred=MISS;
   }
-     else{                        
+     else{
        	X=hash_table_i1[index][i].x; /* set  the current lookup X value */
         hash_table_i1[index][i].x++; /* increase X value */
         /* calculate the return predicted output value and flags */
@@ -490,12 +490,12 @@ struct mem_t *mem  /* The memory block to be accessed for reading the value */ )
 	/* fsm classification:
 	   fsm_pred == 1 => go with prediction
 	   fsm_pred == 0 => don't use prediction */
-        pred_val->fsm_pred=(use_fsm?hash_table_i1[index][i].ps>>1:HIT); 
+        pred_val->fsm_pred=(use_fsm?hash_table_i1[index][i].ps>>1:HIT);
         //update(pred_PC,inst,*pred_val,calc_val,common_ref++);
      }
   //}
   // else{
-  //    if(SS_OP_FLAGS(op)&F_DOUBLE_P){	
+  //    if(SS_OP_FLAGS(op)&F_DOUBLE_P){
   //      /* double presicion */
   //       index = (unsigned int)(pc % hash_no_i2);
   //       /* find entry in the double precision table */
@@ -510,19 +510,19 @@ struct mem_t *mem  /* The memory block to be accessed for reading the value */ )
   //       else{
   //          X=hash_table_i2[index][i].x;  /* set  the current lookuo X value */
   //  	   hash_table_i2[index][i].x++; /* update X for future use */
-       
-	 //     calculate and return predicted output value and flag 
+
+	 //     calculate and return predicted output value and flag
   // 	   val[0]=((en_lookup_stride==0)?hash_table_i2[index][i].stride[0]:X*hash_table_i2[index][i].stride[0]);
   // 	   val[1]=((en_lookup_stride==0)?hash_table_i2[index][i].stride[1]:X*hash_table_i2[index][i].stride[1]);
   //          pred_val->value.double_p[0]=(hash_table_i2[index][i].last_val[0]+((use_stride==0)?0:val[0]));      /*hit*/
   //          pred_val->value.double_p[1]=(hash_table_i2[index][i].last_val[1]+((use_stride==0)?0:val[1]));     /*hit*/
-  //          pred_val->fsm_pred=(use_fsm?hash_table_i2[index][i].ps>>1:HIT); 
+  //          pred_val->fsm_pred=(use_fsm?hash_table_i2[index][i].ps>>1:HIT);
   //       }
   //    }
   //    else return; /* value prediction for other instructions is not used*/
 
-  // /* NOTE: the F_SINGLE_P and F_DOUBLE_P flags should be added to the 
+  // /* NOTE: the F_SINGLE_P and F_DOUBLE_P flags should be added to the
   //          instruction set code (ss.def) for all instructions that you
-	 //   want to be Value Predicted */ 
+	 //   want to be Value Predicted */
   // }
 }
